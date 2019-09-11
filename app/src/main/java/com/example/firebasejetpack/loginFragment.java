497ad4c242e7a02d4900d37c51c7d458747ffe5a
Harsh <hshdave2011@gmail.com>
2019-09-11 17:02:28 -0400

    Login System imple

--- app/src/main/java/com/example/firebasejetpack/loginFragment.java
+++ app/src/main/java/com/example/firebasejetpack/loginFragment.java
@@ -4,14 +4,34 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
-public class loginFragment extends Fragment {
+public class loginFragment extends Fragment implements View.OnClickListener {
+
+    EditText edt_email, edt_pass;
+    Button btn_log;
+    TextView txt_reg;
+
+    private FirebaseAuth auth;
+    FirebaseUser user;
 
     public loginFragment() {
         // Required empty public constructor
@@ -21,6 +41,21 @@ public class loginFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        auth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        edt_email = view.findViewById(R.id.edt_email);
+        edt_pass = view.findViewById(R.id.edt_pass);
+        btn_log = view.findViewById(R.id.btn_log);
+        txt_reg = view.findViewById(R.id.txt_lrge);
+
+        btn_log.setOnClickListener(this);
+        txt_reg.setOnClickListener(this);
 
     }
 
@@ -39,6 +74,47 @@ public class loginFragment extends Fragment {
     }
 
 
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        if (id == R.id.btn_log) {
+            if (TextUtils.isEmpty(edt_email.getText().toString())) {
+                edt_email.setError("Email cannot be blank!");
+                edt_email.requestFocus();
+            } else if (TextUtils.isEmpty(edt_pass.getText().toString())) {
+                edt_pass.setError("Password cannot be blank!");
+                edt_pass.requestFocus();
+            } else {
+                if (edt_pass.getText().toString().length() < 6) {
+                    edt_pass.setError("Password should be at least 6 char!");
+                    edt_pass.requestFocus();
+                } else {
+                    String email = edt_email.getText().toString();
+                    String pass = edt_pass.getText().toString();
+
+                    loginUser(email, pass);
+                }
+            }
 
 
+        } else if (id == R.id.txt_lrge) {
+
+        }
+
+    }
+
+    public void loginUser(String email, String pass) {
+        auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    user = auth.getCurrentUser();
+                    Toast.makeText(getActivity().getApplicationContext(), "Login Success!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getActivity().getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
 }
